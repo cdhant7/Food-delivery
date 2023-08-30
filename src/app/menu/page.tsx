e3eaf33ae0fe0ff8d91bc2708712136bed0345f1
SIDHANT <acharyasid1@gmail.com>
2023-08-30 15:16:30 +0545

    creating pages with nextjs router

---
+++ src/app/menu/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MenuPage = () => {
+  return (
+    <div>
+      MenuPage
+    </div>
+  )
+}
+
+export default MenuPage
+
---
+++ src/app/product/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SingleProductPage = () => {
+  return (
+    <div>
+      SingleProductPage
+    </div>
+  )
+}
+
+export default SingleProductPage
