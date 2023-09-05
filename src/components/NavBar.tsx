a18394d8448af8bfe43c055d36da975b580c09cc
SIDHANT <acharyasid1@gmail.com>
2023-09-06 00:19:19 +0545

    menu and navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,38 +1,42 @@
-import React from 'react'
-import Menu from './Menu'
-import Link from 'next/link'
-import Carticon from './Carticon'
+import React from "react";
+import Menu from "./Menu";
+import Link from "next/link";
+import Carticon from "./Carticon";
+import Image from "next/image";
 
-const NavBar = () => {
-  const user=false
+const Navbar = () => {
+  const user = false;
   return (
-    <div className=' h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase '>
-    {/* Left Links */}
-     <div className='hidden md:flex gap-4 '>
-      <Link href="/">Homepage</Link>
-      <Link href="/menu">Menu</Link>
-      <Link href="/">contact</Link>
-
-     </div>
-      {/* Logo */}
-      <div className=' text-xl '>
-        <Link href="/">CDHANT</Link>
+    <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase md:h-24 lg:px-20 xl:px-40">
+      {/* LEFT LINKS */}
+      <div className="hidden md:flex gap-4 flex-1">
+        <Link href="/">Homepage</Link>
+        <Link href="/menu">Menu</Link>
+        <Link href="/">Contact</Link>
       </div>
-      {/* Mobile Menu */}
-      <div className='md: hidden '>
-        <Menu/>
-        {/* Right Links */}
-        <div className='hidden md:flex gap-4 '></div>
+      {/* LOGO */}
+      <div className="text-xl md:font-bold flex-1 md:text-center">
+        <Link href="/">Cdhant</Link>
+      </div>
+      {/* MOBILE MENU */}
+      <div className="md:hidden">
+        <Menu />
+      </div>
+      {/* RIGHT LINKS */}
+      <div className="hidden md:flex gap-4 items-center justify-end flex-1">
+        <div className="md:absolute top-3 r-2 lg:static flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md">
+          <Image src="/cart.png" alt="" width={20} height={20} />
+          <span>123 456 78</span>
+        </div>
         {!user ? (
           <Link href="/login">Login</Link>
         ) : (
           <Link href="/orders">Orders</Link>
         )}
-
-      <Carticon/>
+        <Carticon />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default Navbar;
