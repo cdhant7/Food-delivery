8585e86218d291c9ddb8af09262e7d7cbd67b66e
SIDHANT <acharyasid1@gmail.com>
2023-09-06 23:57:14 +0545

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className='h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-red-500 flex items-center justify-between '>
+    <Link href='/'>CDHANT</Link>
+    <p>ALL RIGHTS RESERVED</p>
     </div>
   )
 }
