fb833108f21fe56c7bc093cb4e56e400a90edbfe
SIDHANT <acharyasid1@gmail.com>
2023-09-10 23:59:36 +0545

    featured

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -1,9 +1,26 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Featured = () => {
   return (
-    <div>
-      Featured
+    <div className='w-screen overflow-x-scroll text-red-500 '>
+      {/* Wrapper */}
+     <div className='w-max'>
+      {/* Single item */}
+      <div className=''>
+    {/* Image container */}
+    <div className="relative">
+    <Image src="" alt='' fill />
+    </div>
+    {/* text container */}
+    <div className=''>
+      <h1 className=''>title</h1>
+      <p>Desc</p>
+      <span>123</span>
+      <button>Add to Cart</button>
+    </div>
+      </div>
+     </div>
     </div>
   )
 }
