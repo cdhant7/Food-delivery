8b6fb5c8195f82f4eb30ee356f953f88d641e378
SIDHANT <acharyasid1@gmail.com>
2023-09-14 23:57:04 +0545

    category

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -1,9 +1,13 @@
+import { pizzas } from '@/data'
+import Link from 'next/link'
 import React from 'react'
 
 const CategoryPage = () => {
   return (
-    <div>
-      CategoryPage
+    <div className='flex text-red-500'>
+      {pizzas.map(item =>(
+        <Link href={`/product/${item.id}`} key={item.id}></Link>
+      ))}
     </div>
   )
 }
