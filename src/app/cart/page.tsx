d8ea07ede62da89a50f0a519e2dce1a813ea1f95
SIDHANT <acharyasid1@gmail.com>
2023-09-28 23:33:02 +0545

    loginpage

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,9 +1,62 @@
+import Image from 'next/image'
 import React from 'react'
 
 const CartPage = () => {
   return (
-    <div>
-      CartPage
+    <div className='h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col text-red-500 lg:flex-row '>
+      {/* Product container */}
+      <div className=' h-1/2 p-4 flex flex-col justify-center overflow-scroll lg:h-full lg:w-2/3 2xl:w-1/2 lg:px-20 xl:px-40'>
+        {/* single item container */}
+        <div className='flex items-center justify-between mb-4 '>
+          <Image src="/temporary/p1.png" alt='' height={100} width={100}/>
+          <div className=''>
+            <h1 className='uppecase text-xl font-bold' >SILICIAN</h1>
+            <span>Large</span>
+          </div>
+          <h2 className='font-bold' >$79.90</h2>
+          <span className='cursor-pointer' >X</span>
+        </div>
+        <div className='flex items-center justify-between mb-4 '>
+          <Image src="/temporary/p1.png" alt='' height={100} width={100}/>
+          <div className=''>
+            <h1 className='uppecase text-xl font-bold' >SILICIAN</h1>
+            <span>Large</span>
+          </div>
+          <h2 className='font-bold' >$79.90</h2>
+          <span className='cursor-pointer' >X</span>
+        </div>
+        <div className='flex items-center justify-between mb-4 '>
+          <Image src="/temporary/p1.png" alt='' height={100} width={100}/>
+          <div className=''>
+            <h1 className='uppecase text-xl font-bold' >SILICIAN</h1>
+            <span>Large</span>
+          </div>
+          <h2 className='font-bold' >$79.90</h2>
+          <span className='cursor-pointer' >X</span>
+        </div>
+      </div>
+
+      {/* Payment Container */}
+      <div className='h-1/2 p-4 bg-fuchsia-50 flex flex-col gap-4 justify-center lg:h-full lg:w-1/3 2xl:w-1/2 lg:px-20 xl:px-40 2xl:text-xl 2xl:gap-6 '>
+        <div className='flex justify-between'>
+          <span className=''>Subtotal (3Items)</span>
+          <span className=''>$81.70</span>
+        </div>
+        <div className='flex justify-between'>
+          <span className=''>Service Cost</span>
+          <span className=''>$0.00</span>
+        </div>
+        <div className='flex justify-between'>
+          <span className=''>Delivery cost</span>
+          <span className='text-green-500 '>FREE!!</span>
+        </div>
+        <hr className='my-2' />
+        <div className='flex justify-between'>
+          <span className=''>Total(INC.VAT)</span>
+          <span className='font-bold '>$81.70</span>
+        </div>
+        <button className=' bg-red-500 text-white p-3 rounded-md w-1/2 self-end '>CHECKOUT</button>
+      </div>
     </div>
   )
 }
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,34 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const LoginPage = () => {
   return (
     <div>
-        LoginPage
+      {/* BOX */}
+      <div className=''>
+        {/* IMAGE CONTAINER */}
+        <div className='relative'>
+          <Image src="/loginBg.png" alt='' fill />
+        </div>
+        {/* FORM CONTAINER */}
+        <div className=''>
+          <h1>Welcome</h1>
+          <p>Log into your account or create a new one using social button</p>
+          <button className='flex gap-4 p-4 ring-1 ring-orange-100 rounded-md '>
+            <Image src="/google.png " alt='' width={20} height={20} className='object-contain'/>
+            <span>Sign in with Google</span>
+          </button>
+          <button className='flex gap-4 p-4 ring-1 ring-blue-100 rounded-md '>
+            <Image src="/facebook.png " alt='' width={20} height={20} className='object-contain'/>
+            <span>Sign in with Facebook</span>
+          </button>
+          <p>Have a problem? <Link href='/'>Contact us</Link>
+           </p>
+        </div>
+      </div>
     </div>
   )
-}
+} 
 
 export default LoginPage
