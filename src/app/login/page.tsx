03641be02dc78dd86853c90b2b9db29e82f81c08
SIDHANT <acharyasid1@gmail.com>
2023-09-29 23:33:06 +0545

    ready to deploy

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,16 +4,16 @@ import React from 'react'
 
 const LoginPage = () => {
   return (
-    <div>
+    <div className='p-4 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex items-center justify-center '>
       {/* BOX */}
-      <div className=''>
+      <div className='h-full shadow-2xl flex flex-col rounded-md md:flex-row md:h-[70%] md:w-full lg:w-[60%] 2xl:w-1/2 '>
         {/* IMAGE CONTAINER */}
-        <div className='relative'>
-          <Image src="/loginBg.png" alt='' fill />
+        <div className='relative h-1/3 w-full md:h-full md:w-1/2 '>
+          <Image src="/loginBg.png" alt='' fill className='object-cover ' />
         </div>
         {/* FORM CONTAINER */}
-        <div className=''>
-          <h1>Welcome</h1>
+        <div className='p-10 flex flex-col gap-8 md:w-1/2'>
+          <h1 className='font-bold text-xl xl:text-3xl '>Welcome</h1>
           <p>Log into your account or create a new one using social button</p>
           <button className='flex gap-4 p-4 ring-1 ring-orange-100 rounded-md '>
             <Image src="/google.png " alt='' width={20} height={20} className='object-contain'/>
@@ -23,7 +23,8 @@ const LoginPage = () => {
             <Image src="/facebook.png " alt='' width={20} height={20} className='object-contain'/>
             <span>Sign in with Facebook</span>
           </button>
-          <p>Have a problem? <Link href='/'>Contact us</Link>
+          <p className='text-sm ' >
+            Have a problem? <Link className='underline '  href='/'>Contact us</Link>
            </p>
         </div>
       </div>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <div className='h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-red-500 flex items-center justify-between '>
+    <div className='h-12 md:h-24 p-4 lg:px-20 xl:p-40 text-red-500 flex items-center justify-between '>
     <Link href='/' className=' font-bold text-xl '>CDHANT</Link>
     <p>@ ALL RIGHTS RESERVED</p>
     </div>
